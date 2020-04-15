db28762933c94fc69658136dcc57cced8be67453
ppreeth1 <33333408+ppreeth1@users.noreply.github.com>
2020-04-15 12:26:40 -0400

    Tests

---
+++ toddEven.m
@@ -0,0 +1,22 @@
+classdef toddEven < matlab.unittest.Testcase
+       
+    methods(Test)
+        function verifyodd(testcase)
+            X=oddEven(11);
+            testcase.verifyEqual(X,1);
+        end
+        function verifyeven(testcase)
+            X=oddEven(10);
+            testcase.verifyEqual(X,0);
+        end
+        function verifyevenfails(testcase)
+            X=oddEven(11);
+            testcase.verifyEqual(X,0);
+        end
+        function verifyoddfails(testcase)
+            X=oddEven(10);
+            testcase.verifyEqual(X,1);
+        end
+    end
+end
+
